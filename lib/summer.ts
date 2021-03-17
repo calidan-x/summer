743382084e09c809608cee5bc2d05964193d5dc9
Calidan <calidan@vip.qq.com>
2021-03-18 00:05:23 +0800

    fix async bug

--- lib/summer.ts
+++ lib/summer.ts
@@ -6,23 +6,21 @@ import { locContainer } from './loc';
 import { requestMappingAssembler } from './request-mapping';
 
 export const Summer = {
-  loadControllers() {
+  async loadControllers() {
     const fs = require('fs');
     const path = require('path');
-    (async () => {
-      const files = fs.readdirSync('./src/controller');
-      for (let i = 0; i < files.length; i++) {
-        const filePath = path.resolve('./src/controller', files[i]);
-        await import(filePath);
-      }
-    })();
+    const files = fs.readdirSync('./src/controller');
+    for (let i = 0; i < files.length; i++) {
+      const filePath = path.resolve('./src/controller', files[i]);
+      await import(filePath);
+    }
   },
   start() {
     figlet.text('SUMMER', (err, data) => {
       console.log(data);
       console.log('Ver 0.0.1\n');
       (async () => {
-        this.loadControllers();
+        await this.loadControllers();
         await mysqlDB.connect(MYSQL_CONNECT_CONFIG);
         locContainer.initLoc();
         requestMappingAssembler.resolveControllers();
