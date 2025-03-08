e2f8c56c33a9da12936f69a20db7b049f7f6f92f
Calidan <calidan@vip.qq.com>
2025-03-08 13:32:41 +0800

    upgrade version

--- @summer-js/summer/src/cors.ts
+++ @summer-js/summer/src/cors.ts
@@ -5,7 +5,8 @@ const corsHeader = (context: Context) => ({
   'Access-Control-Allow-Origin': context.request.headers['origin'] || '*',
   'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
   'Access-Control-Allow-Headers': context.request.headers['access-control-request-headers'] || '*',
-  'Access-Control-Allow-Credentials': 'true'
+  'Access-Control-Allow-Credentials': 'true',
+  'Access-Control-Max-Age': '604800'
 })
 
 export const handleCors = (ctx: Context) => {
