41da48e5db7f0a27bae82019a40e1e4c53b382e6
Calidan <calidan@vip.qq.com>
2025-07-06 17:57:09 +0800

    upgrade version

--- @summer-js/summer/src/decorators/task.ts
+++ @summer-js/summer/src/decorators/task.ts
@@ -4,6 +4,11 @@ export const Scheduled = (
   cronOrFixedRate: { cron: string; timeZone?: string } | { fixedRate: number }
 ): MethodDecorator => {
   return (target: any, methodName: string) => {
+    if ((cronOrFixedRate as any).cron) {
+      ;(cronOrFixedRate as any).timeZone = (cronOrFixedRate as any).timeZone || Scheduled.defaultTimeZone
+    }
     scheduledTask.add({ class: target.constructor, methodName, cronOrFixedRate })
   }
 }
+
+Scheduled.defaultTimeZone = undefined as string | undefined
